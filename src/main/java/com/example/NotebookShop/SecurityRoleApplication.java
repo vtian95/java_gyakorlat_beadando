616e3307a7686ac712e035f340ec343bb17caab6
kerge94 <101669688+kerge94@users.noreply.github.com>
2022-11-23 18:31:15 +0100

    rosszul ment valami

--- src/main/java/com/example/NotebookShop/SecurityRoleApplication.java
+++ src/main/java/com/example/NotebookShop/SecurityRoleApplication.java
@@ -1,4 +1,4 @@
-package com.example.NotebookShop;
+package com.example.Notebookshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
